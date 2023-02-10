5151d3aa0bfba8cb909a74c4eacbbb7b96b29817
dynamite-bud <r82941037@gmail.com>
2023-02-10 01:20:30 +0000

    Deploying to gh-pages from @ wasmerio/sgp4@bb32ed965fc46735743f7ffc7b7afdd70df6f3a8 🚀

---
+++ implementors/core/fmt/trait.Write.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"wai_bindgen_gen_core":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Write.html\" title=\"trait core::fmt::Write\">Write</a> for <a class=\"struct\" href=\"wai_bindgen_gen_core/struct.Source.html\" title=\"struct wai_bindgen_gen_core::Source\">Source</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
